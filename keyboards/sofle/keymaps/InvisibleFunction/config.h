82d397d4acc402f535f4a4bc2349ac91909182dd
Debian <admin@ip-10-0-0-129.ec2.internal>
2021-11-21 20:28:49 +0000

    Add new logo to left board

--- keyboards/sofle/keymaps/InvisibleFunction/config.h
+++ keyboards/sofle/keymaps/InvisibleFunction/config.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define MASTER_RIGHT
+
+#define OLED_FONT_H "lib/glcdfont_sofle.c"
+
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
